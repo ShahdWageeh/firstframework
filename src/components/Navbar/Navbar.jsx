1c9a7c6ab32208b32e2ef099413f31bf4075e0a0
ShahdWageeh <shahdwageeh79@gmail.com>
2025-02-26 18:30:37 +0200

    third

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,25 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  let [navbar, setNavbar] = useState(100);
-  let changeNav = () => {
-    if (window.scrollY > 40) {
-      setNavbar(80);
-    } else {
-      setNavbar(100);
-    }
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", changeNav);
-
-    return () => {
-      window.removeEventListener("scroll", changeNav);
-    };
-  }, []);
   return (
     <>
       <nav
-        style={{ height: `${navbar}px`, transition: "height 0.3s ease" }}
         className="navbar navbar-expand-lg fixed-top py-4"
       >
         <div className="container">
@@ -36,13 +20,13 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <i className="fa-solid fa-bars fs-3 text-white"></i>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink
-                  className="nav-link fw-bold text-white me-3 d-inline-block"
+                  className="nav-link fw-bold text-white me-3 d-inline-block px-3"
                   aria-current="page"
                   to="/about"
                 >
@@ -51,7 +35,7 @@ export default function Navbar() {
               </li>
               <li className="nav-item">
                 <NavLink
-                  className="nav-link fw-bold me-3 text-white d-inline-block"
+                  className="nav-link fw-bold me-3 text-white d-inline-block px-3"
                   aria-current="page"
                   to="/portfolio"
                 >
@@ -60,7 +44,7 @@ export default function Navbar() {
               </li>
               <li className="nav-item">
                 <NavLink
-                  className="nav-link fw-bold text-white d-inline-block"
+                  className="nav-link fw-bold text-white d-inline-block px-3"
                   aria-current="page"
                   to="/contact"
                 >
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,6 +9,16 @@ export default function Portfolio() {
   useEffect(()=>{
     document.title = 'Portfolio'
   },[])
+  const [ShowModal, setShowModal] = useState(false)
+  const [ModalImg, setModalImg] = useState('')
+  function openModal(img){
+    setShowModal(true)
+    setModalImg(img)
+  }
+  function closeModal(){
+    setShowModal(false)
+    setModalImg('')
+  }
   let locationS = window.location.pathname;
   return (
     <>
@@ -19,9 +29,10 @@ export default function Portfolio() {
             <StarLine loc={locationS} />
           </div>
           <div className="row gy-4 mt-3 pb-4">
-            <div className="col-md-6 col-lg-4">
-              <div className={`${styles.inner} position-relative`}>
-                <img src={pic1} alt="" className=" img-fluid rounded-2" />
+            {[pic1, pic2, pic3, pic1, pic2, pic3].map((pic, index)=>(
+            <div className="col-md-6 col-lg-4" key={index}>
+              <div onClick={()=>openModal(pic)} className={`${styles.inner} position-relative`}>
+                <img src={pic} alt="" className=" img-fluid rounded-2" />
                 <div
                   className={`${styles.layer} d-flex justify-content-center align-items-center`}
                 >
@@ -29,59 +40,18 @@ export default function Portfolio() {
                 </div>
               </div>
             </div>
-            <div className="col-md-6 col-lg-4">
-              <div className={`${styles.inner} position-relative`}>
-                <img src={pic2} alt="" className=" img-fluid rounded-2" />
-                <div
-                  className={`${styles.layer} d-flex justify-content-center align-items-center`}
-                >
-                  <i className="fa-solid fa-plus fa-8x text-white"></i>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <div className={`${styles.inner} position-relative`}>
-                <img src={pic3} alt="" className=" img-fluid rounded-2" />
-                <div
-                  className={`${styles.layer} d-flex justify-content-center align-items-center`}
-                >
-                  <i className="fa-solid fa-plus fa-8x text-white"></i>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <div className={`${styles.inner} position-relative`}>
-                <img src={pic1} alt="" className=" img-fluid rounded-2" />
-                <div
-                  className={`${styles.layer} d-flex justify-content-center align-items-center`}
-                >
-                  <i className="fa-solid fa-plus fa-8x text-white"></i>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <div className={`${styles.inner} position-relative`}>
-                <img src={pic2} alt="" className=" img-fluid rounded-2" />
-                <div
-                  className={`${styles.layer} d-flex justify-content-center align-items-center`}
-                >
-                  <i className="fa-solid fa-plus fa-8x text-white"></i>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-4">
-              <div className={`${styles.inner} position-relative`}>
-                <img src={pic3} alt="" className=" img-fluid rounded-2" />
-                <div
-                  className={`${styles.layer} d-flex justify-content-center align-items-center`}
-                >
-                  <i className="fa-solid fa-plus fa-8x text-white"></i>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
+      {ShowModal && (
+        <div className={`${styles.modal}`} onClick={closeModal}>
+          <div className={`${styles.modalContent} w-50 pt-5 mt-5`} onClick={(e) => e.stopPropagation()}>
+            <img src={ModalImg} alt="selected" className="img-fluid" />
+            {/* <button className="btn-close" onClick={closeModal}></button> */}
+          </div>
+        </div>
+      )}
     </>
   );
 }
